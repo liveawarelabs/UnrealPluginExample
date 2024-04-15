c2f8f91e28d341e40b5e945fc5dd3f69b573843f
Chris Idzerda <152215433+lal-chris@users.noreply.github.com>
2024-04-15 12:45:37 -0700

    Add MyBlueprintFunctionLibrary class
    This came as a result of selecting "Add C++ class" from the "Tools" menu
    and then selecting "Blueprint Function Library" as the base class.

---
+++ Source/UnrealPluginExample/MyBlueprintFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MyBlueprintFunctionLibrary.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class UNREALPLUGINEXAMPLE_API UMyBlueprintFunctionLibrary : public UBlueprintFunctionLibrary {
+	GENERATED_BODY()
+	UFUNCTION(BlueprintCallable, Category = "Live Aware")
+	static void MakeConnection();
+};
---
+++ Source/UnrealPluginExample/UnrealPluginExample.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealPluginExample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, UnrealPluginExample, "UnrealPluginExample");
