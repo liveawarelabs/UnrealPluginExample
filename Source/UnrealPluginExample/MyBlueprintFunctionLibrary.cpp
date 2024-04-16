2f03f30f11e4de3f8c0dae5147e4c85539580296
Chris Idzerda <152215433+lal-chris@users.noreply.github.com>
2024-04-16 14:28:48 -0700

    Make a connection and remove all widgets

--- Source/UnrealPluginExample/MyBlueprintFunctionLibrary.cpp
+++ Source/UnrealPluginExample/MyBlueprintFunctionLibrary.cpp
@@ -1,5 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MyBlueprintFunctionLibrary.h"
+#include <memory>
+#include <RecorderPlugin.h>
 
-void UMyBlueprintFunctionLibrary::MakeConnection() {}
+namespace {
+	std::unique_ptr<LiveAwareLabs::RecorderPlugin> plugin;
+}
+
+void UMyBlueprintFunctionLibrary::MakeConnection() {
+	plugin = LiveAwareLabs::RecorderPlugin::Create();
+}
